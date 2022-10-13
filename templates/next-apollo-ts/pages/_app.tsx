0d4ecd7884b0e2734ba2513a42085f352a676018
Mario Estrada <mario.estrada@sequra.es>
2022-10-13 09:39:32 +0200

    feat(template): adds a new tempalte for Nextjs with Apollo with Typescript

---
+++ templates/next-apollo-ts/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const apolloClient = useApollo(pageProps)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
+
+export default App
